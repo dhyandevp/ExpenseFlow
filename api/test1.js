f423e93c78eceb2be630a4499a4580c6a41cc9ac
dhyandevp <dhyandev027@gmail.com>
2026-08-16 13:12:51 +0530

    Test dynamic import

--- api/test1.js
+++ api/test1.js
@@ -1,2 +1,8 @@
-import { getAuth } from 'firebase-admin/auth';
-export default function handler(req, res) { res.status(200).json({ ok: 1 }) }
+export default async function handler(req, res) {
+  try {
+    const authModule = await import('firebase-admin/auth');
+    res.status(200).json({ ok: 1, type: typeof authModule.getAuth });
+  } catch (err) {
+    res.status(500).json({ error: String(err), stack: err.stack });
+  }
+}
